2370c22754eafafec713258ef12319960df14872
Jason Wu <30715413+Jason-S-Wu@users.noreply.github.com>
2024-04-03 23:03:34 -0400

    added mock market data from v1

---
+++ constants/mock-data/MockMarketData.tsx
@@ -0,0 +1,28 @@
+export const marketData = [
+  {
+    fileHash: 'QmXp4FGDzFbPzcypwA6rLf1LuA2jT2Z3MgJYVJuY8rhJFm',
+    name: 'Avatar',
+    size: 2000, // 2 GB
+  },
+  {
+    fileHash: 'QmV5T6Nw8SCFKe3t7H8KZuTyuX5DmLZ39uMMu5oBDHZFfv',
+    name: 'The Dark Knight',
+    size: 1800, // 1.8 GB
+  },
+  // {
+  //   fileHash: 'QmUx6Qe3c56JpV93HZqo51FUsXbt7YvLpcRUPFDK5ewFLd',
+  //   name: 'Inception',
+  //   size: 2200, // 2.2 GB
+  // },
+  // {
+  //   fileHash: 'QmUx6Qe3c56JpV93HZqo51FUsXbt7YvLpcRUPFDK5ewFL3',
+  //   name: 'Barbie',
+  //   size: 2500, // 2.5 GB
+  // },
+  // {
+  //   fileHash: 'QmUx6Qe3c56JpV93HZqo51FUsXbt7YvLpcRUPFDK5ewFL4',
+  //   name: 'Minions',
+  //   size: 2200, // 1.2 GB
+  // },
+  // Add more mock files as needed
+];
