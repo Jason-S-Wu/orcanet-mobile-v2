a092f7bdc34f63d184a1341291f53e99e919ab65
z-ashley <52079959+z-ashley@users.noreply.github.com>
2024-04-06 12:05:28 -0400

    viewer back button

--- app/viewer/[fileName].tsx
+++ app/viewer/[fileName].tsx
@@ -1,14 +1,23 @@
-import {View} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {Text} from 'react-native-paper';
-import {useLocalSearchParams} from 'expo-router';
+import {Button, Text} from 'react-native-paper';
+import {useLocalSearchParams, useNavigation} from 'expo-router';
 
 const Viewer = () => {
-  const {fileName} = useLocalSearchParams<{fileName: string}>();
+  const { fileName } = useLocalSearchParams<{ fileName: string }>();
+  const navigation = useNavigation();
+
+  const handleBackPress = () => {
+    navigation.goBack(); 
+  };
+
   return (
     <View>
       <Text>Viewer</Text>
       <Text>{fileName} is being viewed.</Text>
+      <Button onPress={handleBackPress}  style={{ marginTop: 10 }}>
+        <Text>Go back to file page</Text>
+      </Button>
     </View>
   );
 };
