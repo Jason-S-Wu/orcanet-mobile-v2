a3480dfbf749930eb75c72c2fb76103cb6520f96
z-ashley <52079959+z-ashley@users.noreply.github.com>
2024-04-21 21:49:26 -0400

    style edit

--- app/(modals)/settings.tsx
+++ app/(modals)/settings.tsx
@@ -1,5 +1,5 @@
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import React, { useState }  from 'react';
+import React, {useState}  from 'react';
 
 const Settings = () => {
   const [generatedKey, setGeneratedKey] = useState<string|null>(null);
